65f396d8c395ff6543f1d182bcf39c970735d916
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-03-26 23:20:54 -0700

    Implemented hexdump, and switched base64 to a separate file

--- projects/proj1.c
+++ projects/proj1.c
@@ -3,104 +3,104 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BYTE_SIZE 8
-#define BASE64_BYTE_SIZE 6
+#include "base64.h"
 
-unsigned char base64_ascii_to_char(char input) {
-  if (input >= 0 && input <= 25) {
-    return input + 'A';
-  } else if (input >= 26 && input <= 51) {
-    return input + 'a' - 26;
-  } else if (input >= 52 && input <= 61) {
-    return input + '0' - 52;
-  } else if (input == 62) {
-    return '+';
-  } else if (input == 63) {
-    return '/';
-  } else {
-    printf("Unknown base64 conversion from character [%c]\n", input);
-    return 0;
-  }
-}
+#define BYTES_PER_ROW 16
+#if BYTES_PER_ROW % 2 == 0
+#define BYTES_HALFWAY BYTES_PER_ROW / 2
+#else
+#error "Bytes per row is not a power of two"
+#endif
 
 void hexdump_data(const char data[], int data_size) {
-  int num_columns = data_size / 16;
-  if (num_columns % 8 != 0) {
-    num_columns += 1;
-  }
+  int row_counter = 0;
+  for (int row = 0; row < data_size; row += BYTES_PER_ROW) {
+    // Print out the row counter
+    printf("%.6x: ", row_counter);
 
-  for (int ri = 0; ri < num_columns; ri++) {
-    int start_index = 2 * ri;
-    printf("Column %d starts at index %d and ends at %d\n", ri, start_index, 0);
+    // Print the hex of every 16 bytes
+    for (int current_byte = 0; current_byte < BYTES_PER_ROW; current_byte++) {
+      // Replace anything blank with padding characters
+      if (row + current_byte >= data_size) {
+        printf("-- ");
+      } else {
+        printf("%.2x ", data[row + current_byte]);
+      }
+
+      // Put a space halfway in between both columns
+      if (current_byte == (BYTES_HALFWAY - 1)) {
+        printf("  ");
+      }
+    }
+
+    // Put just once space because all the hex values have a space after them
+    printf(" ");
+
+    // Print the string representation of the bits
+    for (int current_byte = 0; current_byte < BYTES_PER_ROW; current_byte++) {
+      char character = data[row + current_byte];
+
+      switch (character) {
+        // Only print out the characters that work
+      case 'a' ... 'z':
+      case 'A' ... 'Z':
+        printf("%c", character);
+        break;
+        // Print a filler character for everything that can't be printed
+      default:
+        printf(".");
+        break;
+      }
+    }
+
+    // Go to the next line
+    printf("\n");
+    row_counter += BYTES_PER_ROW;
   }
 }
 
 int main(void) {
-  char *input = "Hi\n";
+  char *input = "Hi";
   int input_len = strlen(input);
 
-  int num_bits = sizeof(char) * input_len * 8;
-  int num_blocks = num_bits / BASE64_BYTE_SIZE;
-
-  bool needs_padding = num_bits % BASE64_BYTE_SIZE != 0;
-  // Division always rounds down, so if we need padding, we're always going to
-  // be one block short
-  if (needs_padding) {
-    num_blocks += 1;
-  }
-
-  // Create an array to store the blocks
-  // We don't know the size of this until we calculate it, so it has to live
-  // on the heap
-  char *output_str = malloc(sizeof(char) * num_blocks);
-  int output_index = 0;
-  if (output_str == NULL) {
-    printf("Allocation of array failed!\n");
+  FILE *fd = fopen("test1.txt", "rb");
+  if (fd == NULL) {
+    printf("Error accessing file\n");
     return 1;
   }
 
-  char temp_byte = 0;
+  // Get the size of the file in bytes
+  fseek(fd, 0, SEEK_END);
+  int file_size = ftell(fd);
+  rewind(fd);
 
-  // Loop through each bit in the input
-  for (int i = 0; i < num_bits; i++) {
-    // This is the array index that the current bit is in
-    int data_index = i / BYTE_SIZE;
-    // `reverse_bit` extracts the ith bit of the current byte, starting from the
-    // most significant bit
-    int reverse_bit = (input[data_index] &
-                       (0x01 << ((BYTE_SIZE - 1) - (i % BYTE_SIZE)))) != 0;
-    // `temp_shift` is the index of the bit in the base64 byte
-    int temp_shift = (BASE64_BYTE_SIZE - 1) - (i % BASE64_BYTE_SIZE);
-    // Set that bit to the extracted bit
-    temp_byte |= reverse_bit << temp_shift;
-    // Once we've filled up a full base64 byte, add it to the array
-    if (i % BASE64_BYTE_SIZE == (BASE64_BYTE_SIZE - 1)) {
-      output_str[output_index++] = temp_byte;
-      // Reset it as well
-      temp_byte = 0;
-    }
+  char *file_buffer = (char *)malloc(sizeof(char) * file_size);
+  if (file_buffer == NULL) {
+    printf("Error allocating memory for file\n");
+    return 1;
   }
 
-  // If the result was padded, then we'll need to treat the in-progress base64
-  // byte as a full byte, and it's already zero-padded
-  if (needs_padding) {
-    output_str[output_index] = temp_byte;
+  int bytes_read = fread(file_buffer, 1, file_size, fd);
+  if (bytes_read != file_size) {
+    printf("Error reading file, tried to read %d bytes, got %d\n", file_size,
+           bytes_read);
+    return 1;
   }
 
-  // Print out the character encoding of every base64 byte
-  for (int bi = 0; bi < num_blocks; bi++) {
-    // printf("%.6b ", output_str[bi]);
-    printf("%c", base64_ascii_to_char(output_str[bi]));
+  fclose(fd);
+
+  hexdump_data(file_buffer, file_size);
+
+  free(file_buffer);
+
+  char *encoded = base64_encode(input, input_len);
+  if (encoded == NULL) {
+    printf("Error encoding base64 data");
+    return 1;
   }
 
-  // Pad the rest until the block length is a multiple of 4
-  while (num_blocks++ % 4 != 0) {
-    // printf("%.6b ", 0x0);
-    printf("=");
-  }
+  printf("Base64 encoded %s -> %s\n", input, encoded);
+  free(encoded);
 
-  printf("\n");
-
-  free(output_str);
   return 0;
 }
