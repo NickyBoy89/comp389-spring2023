f6227592ad5e378e394b23071bd2f66a8ed73dd9
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-04-10 07:57:02 -0700

    Moved projects to their own folders, and added most of the functionality

--- projects/base64.h
+++
@@ -1,5 +0,0 @@
-// `base64_encode` takes in an array of bytes and returns a pointer to its base64
-// string representation
-//
-// Callers should free the string returned by this function after use
-char *base64_encode(const char *data, int data_size);
